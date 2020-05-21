ede7a4e84ec62759cdbc896e2f3ae58c8533d711
andydlindsay@gmail.com <andydlindsay@gmail.com>
2020-05-20 22:22:43 -0700

    initial commit

---
+++ class-based/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+const App = () => {
+  return (
+    <div className="App">
+      <h1>Class-Based Components!</h1>
+    </div>
+  );
+};
+
+export default App;
